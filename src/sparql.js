6f2e13143d2b85bb2df3de809daff438492b5954
Michael Luggen <github.michael@oiu.ch>
2017-07-31 16:38:49 +0200

    tests, error handling

--- src/sparql.js
+++ src/sparql.js
@@ -1,14 +1,20 @@
 import {request} from 'd3-request'
 
+const xmlSchema = "http://www.w3.org/2001/XMLSchema#";
+
 export default function (url, query, callback) {
   var url = url + '?query=' + encodeURIComponent(query)
 
   var response  = function (xhr) {
-        var body = JSON.parse(xhr.responseText)
+        try {
+          var body = JSON.parse(xhr.responseText)
+        } catch (e) {
+          throw new Error("unable to parse response, either the Endpoint URL is wrong or the Endpoint does not answer with sparql-results+json: " + xhr.responseText)
+        }
         return body.results.bindings.map(function(row) {
           var rowObject = {}
           Object.keys(row).forEach(function (column) {
-            rowObject[column] = row[column].value
+            rowObject[column] = dataTypeCasting(row[column])
           })
           return rowObject
         })
@@ -18,9 +24,45 @@ export default function (url, query, callback) {
 
   if (callback != null) {
     if (typeof callback !== "function") throw new Error("invalid callback: " + callback);
+
     return sparql.get(callback)
   }
 
   return sparql
 
 };
+
+export function dataTypeCasting(value) {
+  var v = value.value;
+  if (typeof value.datatype !== "undefined") {
+    var dt = value.datatype.replace(xmlSchema,'')
+    switch(dt) {
+      case 'string':
+        v = new String(v); break;
+      case 'boolean':
+        v = new Boolean(v == "false" ? false : v); break;
+      case 'float':
+      case 'integer':
+      case 'long':
+      case 'double':
+      case 'decimal':
+      case 'nonPositiveInteger':
+      case 'nonNegativeInteger':
+      case 'negativeInteger':
+      case 'int':
+      case 'unsignedLong':
+      case 'positiveInteger':
+      case 'short':
+      case 'unsignedInt':
+      case 'byte':
+      case 'unsignedShort':
+      case 'unsignedByte':
+        v = new Number(v); break;
+      case 'date':
+      case 'time':
+      case 'dateTime':
+        v = new Date(v); break;
+    }
+  }
+  return v;
+}
--- test/sparql-test.js
+++ test/sparql-test.js
@@ -3,7 +3,13 @@ var tape = require("tape"),
 
 // endpoint vendor: blazegraph
 var wikidataUrl = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql'
+// endpoint vendor: stardog
+var adminUrl = 'http://data.admin.ch/query'
+// endpoint vendor: virtuoso
+var ldgeoadminUrl = 'https://ld.geo.admin.ch/query'
 
+
+//*******
 var mikeQuery = `#D3.js Author
 SELECT ?developerName WHERE {
   wd:Q3011087 wdt:P178 ?developer.
@@ -11,24 +17,74 @@ SELECT ?developerName WHERE {
   FILTER(LANG(?developerName) = 'en')
 }
 `
-var catQuery = `#Cats
-SELECT ?item ?itemLabel WHERE {
-  ?item wdt:P31 wd:Q146.
-  ?item rdfs:label ?itemLabel.
-  FILTER(LANG(?itemLabel) = 'en')
-}
-`
 
-tape("sparql(wikidataUrl, mikeQuery) returns the developer of D3.js", function(test) {
+tape("Simple Query returns the developer of D3.js [Blazegraph] [https]", function(test) {
   sparql.sparql(wikidataUrl, mikeQuery, function(error, data) {
     test.equal(data[0].developerName, 'Mike Bostock');
+    test.error(error);
   })
   test.end();
 });
 
-tape("sparql(wikidataUrl, catQuery) returns what the internet was build for, obviously.", function(test) {
-  sparql.sparql(wikidataUrl, catQuery, function(error, data) {
-    test.equal(data.length, 113);
+
+//*******
+var swissMuniQuery = `SELECT ?id ?name ?canton WHERE {
+  ?municipality a <https://gont.ch/MunicipalityVersion>.
+  ?municipality <https://gont.ch/id> ?id.
+  ?municipality <https://gont.ch/longName> ?name.
+  ?municipality <https://gont.ch/canton> ?cantonIri.
+  ?cantonIri <https://gont.ch/longName> ?canton.
+} ORDER BY ?id`
+
+tape("Larger response [Stardog] [http]", function(test) {
+  sparql.sparql(adminUrl, swissMuniQuery, function(error, data) {
+    test.deepEqual(data[0].id, new Number(10001))
+    test.error(error);
+  })
+  test.end();
+});
+
+
+//*******
+var swissCantonsQuery = `SELECT ?Canton ?CantonName
+WHERE {
+  ?Canton a <http://www.geonames.org/ontology#A.ADM1> .
+  ?Canton a <http://schema.org/AdministrativeArea> .
+  ?Canton <http://schema.org/name> ?CantonName .
+}`
+
+tape("Query with known amount of results [Virtuoso] [https]", function(test) {
+  sparql.sparql(ldgeoadminUrl, swissCantonsQuery, function(error, data) {
+    test.equal(data.length, 26);
+  })
+  test.end();
+});
+
+
+//*******
+//Errors
+
+tape("Existing domain, not a SPARQL Endpoint.", function(test) {
+  sparql.sparql('http://example.com', mikeQuery, function(error, data) {
+    test.equal(data, undefined)
+    test.equal(error !== null, true)
+  })
+  test.end();
+});
+
+tape("Non-existing domain.", function(test) {
+  sparql.sparql('http://notexisting.example.com', mikeQuery, function(error, data) {
+    test.equal(data, undefined)
+    test.equal(error !== null, true)
+  })
+  test.end();
+});
+
+
+tape("Syntax error in SPARQL Query.", function(test) {
+  sparql.sparql(wikidataUrl, 'SELECT * WHERE {s?s ?p ?o.} LIMIT 10').get(function(error, data) {
+    test.equal(data, undefined)
+    test.equal(error !== null, true)
   })
   test.end();
 });
