bf51f3967114e59fa1727000dc460da9c7f1d91f
Victor Felder <victor@draft.li>
2019-04-16 17:32:41 +0200

    chore: fix the mix of semis/no semis; remove primitive types constructors; tasks reorg

--- src/sparql.js
+++ src/sparql.js
@@ -1,17 +1,17 @@
-import {request} from 'd3-request'
+import { request } from 'd3-request'
 
-const xmlSchema = "http://www.w3.org/2001/XMLSchema#";
+const xmlSchema = 'http://www.w3.org/2001/XMLSchema#'
 
 export default function (endpoint, query, callback) {
   var url = endpoint + '?query=' + encodeURIComponent(query)
 
-  var response  = function (xhr) {
+  var response = function (xhr) {
     try {
       var body = JSON.parse(xhr.responseText)
     } catch (e) {
-      throw new Error("unable to parse response, either the Endpoint URL is wrong or the Endpoint does not answer with sparql-results+json: " + xhr.responseText)
+      throw new Error('unable to parse response, either the Endpoint URL is wrong or the Endpoint does not answer with sparql-results+json: ' + xhr.responseText)
     }
-    return body.results.bindings.map(function(row) {
+    return body.results.bindings.map(function (row) {
       var rowObject = {}
       Object.keys(row).forEach(function (column) {
         rowObject[column] = dataTypeCasting(row[column])
@@ -23,24 +23,23 @@ export default function (endpoint, query, callback) {
   var sparql = request(url).mimeType('application/sparql-results+json').response(response)
 
   if (callback != null) {
-    if (typeof callback !== "function") throw new Error("invalid callback: " + callback);
+    if (typeof callback !== 'function') throw new Error('invalid callback: ' + callback)
 
     return sparql.get(callback)
   }
 
   return sparql
-
 };
 
-function dataTypeCasting(value) {
-  var v = value.value;
+function dataTypeCasting (value) {
+  var v = value.value
   if (typeof value.datatype === 'string') {
-    var dt = value.datatype.replace(xmlSchema,'')
-    switch(dt) {
+    var dt = value.datatype.replace(xmlSchema, '')
+    switch (dt) {
       case 'string':
-        v = new String(v); break;
+        v = String(v); break
       case 'boolean':
-        v = new Boolean(v == "false" ? false : v); break;
+        v = Boolean(v === 'false' ? false : v); break
       case 'float':
       case 'integer':
       case 'long':
@@ -57,12 +56,12 @@ function dataTypeCasting(value) {
       case 'byte':
       case 'unsignedShort':
       case 'unsignedByte':
-        v = new Number(v); break;
+        v = Number(v); break
       case 'date':
       case 'time':
       case 'dateTime':
-        v = new Date(v); break;
+        v = Date(v); break
     }
   }
-  return v;
+  return v
 }
